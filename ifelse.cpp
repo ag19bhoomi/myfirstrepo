cb2fb620e52bc8e09c4fa016abcd18a0769cd177
bhoomi <bhoomiagrawal175@gmail.com>
2023-12-23 16:13:08 +0530

    ifelse.cpp is added

---
+++ ifelse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout<< "Enter your age:"<<endl;
+		cin>>age;
+	if(age>18)
+		cout<<"You are elegible to vote"<<endl;
+	else
+		cout<<"You are not elegible to vote"<<endl;
+
+return 0;
+}
