8e8587c695d522dc5daae54ccd35069fbfaff7df
bhoomi <bhoomiagrawal175@gmail.com>
2023-12-23 23:10:30 +0530

    inline.cpp file is added

---
+++ inline.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+inline int product(int a,int b);
+int main ()
+{
+	int a,b;
+	cout<<"Enter the value of a and b:"<<endl;
+	cin>>a>>b;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	cout<<"The product of a and b is: "<<product(a,b)<<endl;
+	return 0;
+}
+
+inline int product(int a, int b)
+{
+	return a*b;
+}
