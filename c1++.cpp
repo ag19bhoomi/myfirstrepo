08783865267a90970322d8064295e89348fc5442
bhoomi <bhoomiagrawal175@gmail.com>
2023-12-23 15:32:49 +0530

    c1++.cpp is added

---
+++ c1++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int num1,num2;
+	cout<<"Enter the value of num1:"<<endl;
+        cin>>num1;
+
+
+	cout<<"Enter the value of num2:"<<endl;
+        cin>>num2;
+
+	cout<<"The sum is:"<<num1+num2<<endl;
+        
+	return 0;
+}
