bb7c6a47673ee3f25d2887e077f0b69e1abc7f5d
bhoomi <bhoomiagrawal175@gmail.com>
2023-12-25 00:38:35 +0530

    constructor.cpp is modified

--- constructor.cpp
+++ constructor.cpp
@@ -3,25 +3,69 @@ using namespace std;
 
 class Hero
 {
+	private:
+		int health;
 	public:
+		char level;
+                  
+		
 		Hero()
+		  {
+			  cout<<"constructor called"<<endl;
+		  }
+
+
+                 //paramerterised constructor
+		 Hero(int health)
+		 {  
+			 cout << "this -> " << this <<endl;
+			 this ->health = health;
+		 }
+		 Hero(int health , char level)
+		 {
+			 this -> level = level;
+			 this -> health = health;
+
+		 }
+		void print()
 		{
-			cout << "contructor called" << endl;
+			cout<<level<<endl;
+		}
+
+		int getHealth()
+		{
+			return health;
+		}
+
+		char getLevel()
+		{
+			return level;
+		}
+
+		void setHealth(int h)
+		{
+			health = h;
+		}
+
+		void setLevel(char ch)
+		{
+			level = ch;
 		}
 
 };
 
-
 int main()
 {
-	//statically object created
-	cout<<"HIII"<<endl;
-	Hero bhoomi;
-	cout<<"hello"<<endl;
-        
-	//dynamically
-	Hero *h = new Hero;
+	//static allocation
+        Hero a(20);
+       //cout << "address of a: "<< &a<< endl;
+         a.print();
+
+	//dynamically 
+	Hero *h = new Hero(20);
+        h->print();
+
+	Hero temp(22, 'B');
+	temp.print();
 	
-	
-	return 0;
-}
+return 0;}
