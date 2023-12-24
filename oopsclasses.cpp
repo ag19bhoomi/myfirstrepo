1dfc4b3d968aab9cda6341ce107334673a644072
bhoomi <bhoomiagrawal175@gmail.com>
2023-12-24 17:49:35 +0530

    oopsclasses.cpp file is modified

--- oopsclasses.cpp
+++ oopsclasses.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class employee
+class hero
 {
-	private:
-		int a,b,c;
-	public:
-			int d,e;
-		void setdata(int a1,int b1,int c1);  //declaration
-			void getdata()
-			{
-				cout<<"The value of a is: "<<a<<endl;
-				cout<<"The value of b is: "<<b<<endl;
-				cout<<"The value of c is: "<<c<<endl;
-				cout<<"The value of d is: "<<d<<endl;
-				cout<<"The value of e is: "<<e<<endl;
-			}
+  //properties
+  
+	int health;
+	char name[10];
+	float height;
 };
-
-void employee :: setdata(int a1, int b1, int c1)
-{
-	a=a1;
-	b=b1;
-	c=c1;
-}
 
 int main()
 {
-	employee harry;
-	//harry.a=52; (this will show error as a is private variable)
-	harry.d=12;
-	harry.e=52;
-	harry.setdata(1,8,6);
-	harry.getdata();
+	//creation of object
+	hero h1;
+
+	cout <<"size is: "<<sizeof(h1) << endl;
+
 	return 0;
 }
