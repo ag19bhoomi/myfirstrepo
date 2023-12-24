98f6bbbffd472c7d1b189b8bcc3fc7e66b3d6de8
bhoomi <bhoomiagrawal175@gmail.com>
2023-12-24 18:12:22 +0530

    oopsclasses.cpp file is modified

--- oopsclasses.cpp
+++ oopsclasses.cpp
@@ -6,8 +6,7 @@ class hero
   //properties
    public:                    //they can be used outside class
 	int health;
-   private:                    //they can only be used inside class
-	char name[10];
+	char name;
 	
 	void print()
 	{
@@ -21,6 +20,8 @@ int main()
 {
 	//creation of object
 	hero h1;
+	 h1.health = 90;
+	 h1.name = 'A';
 
 	cout <<"health is: "<<h1.health<<endl;
 
