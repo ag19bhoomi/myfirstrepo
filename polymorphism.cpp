3e4902bfce79c50cee6abe06bfb10cca3b0f5cf7
bhoomi <bhoomiagrawal175@gmail.com>
2024-01-08 21:16:25 +0530

    polymorphism.cpp is modified

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -3,25 +3,20 @@
 //funtion overloading.
 //operator overloading
 
-//Function overloading with different types of arguments
+//Function with default arguments can be called 2 or 3 or 4 arguments.
 #include<iostream>
 using namespace std;
-//Function with two integers parameters
-int add(int num1, int num2)
-{
-	return num1 + num2;
-}
 
-//Function with two double parameters
-double  add(double num1, double num2)
+int add(int x, int y, int z=0, int w =0)
 {
-	return num1+num2;
+	return (x+y+z+w);
 }
 
 int main()
 {
 	cout << add(10,20) <<endl;
-	cout << add(10.4,20.5) <<endl;
+	cout << add(10,20, 30) <<endl;
+	cout << add(10,20,30,40) <<endl;
 	return 0;
 }
 
