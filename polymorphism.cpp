236cb09b7413c019950c5727da3149ad7fd2c732
bhoomi <bhoomiagrawal175@gmail.com>
2024-01-08 21:38:35 +0530

    polymorphism.cpp is modified

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,27 +1,46 @@
 //************POLYMORPHISM*****************
-//compile time:
-//funtion overloading.
+//Types of polymorphism
+//1.compile time (static poly.):
+//a.funtion overloading.
+//b.operator overloading
+//2. Run time (dynamic poly.):
+// it depends on inheritance.
 
-
-
-//operator overloading
+//a.Method overloading.
 #include<iostream>
 using namespace std;
 
-class B
+class parent
 {
 	public:
-	        void operator() ()
+	        void show() 
 		{
-			cout<< "HELLO BHOOMI" <<endl;
+			cout<< "Inside parent class" <<endl;
                 }
+};
+
+class subclass1: public parent
+{
+	public: void show()
+		{
+			cout << "Inside subclass1" << endl;
+		}
+};
+class subclass2: public parent
+{
+	public:
+		void show()
+		{
+			cout<< "inside subclass2"<<endl;
+		}
 };
 
 int main()
 {
-	B obj1;
-
-	obj1();
+	subclass1 bh1;
+	subclass2 bh2;
+	bh1.show();
+	bh2.show();
 	return 0;
 }
 
