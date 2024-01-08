cf26bf1da6309c656ad4d02a15197583ea810a06
bhoomi <bhoomiagrawal175@gmail.com>
2024-01-08 20:08:04 +0530

    polymorphism.cpp is added

---
+++ polymorphism.cpp
@@ -0,0 +1,27 @@
+//************POLYMORPHISM*****************
+//compile time:
+//funtion overloading.
+//operator overloading
+
+//Function overloading with different no.s of arguments
+#include<iostream>
+using namespace std;
+//Function with two parameters
+int add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+//function with three parameters
+int add(int num1 + num2 +num3)
+{
+	return num1+num2+num3
+}
+
+int main()
+{
+	cout << add(10,20) <<endl;
+	cout << add(10,20,30);
+	return 0;
+}
+
